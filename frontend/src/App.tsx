1952e6a092b8350bc50dd793b42ecce84dc7c5e4
Theo <theoslater1@gmail.com>
2024-09-24 18:17:01 +0100

    django and react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React with MUI!</h1>
+      <Button variant="contained">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
